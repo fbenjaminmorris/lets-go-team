a3d6f29a37727e1661011ca08d27343220384392
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-11 21:25:04 -0400

    update

--- index.js
+++ index.js
@@ -11,14 +11,12 @@ var connection = mysql.createConnection({
   database: "employeesDB"
 });
 
-// connect to the mysql server and sql database
 connection.connect(function (err) {
     if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    firstPrompt();
+      firstPrompt();
   });
   
-  // function which prompts the user for what action they should take
+  
   function firstPrompt() {
   
     inquirer
@@ -29,12 +27,61 @@ connection.connect(function (err) {
         choices: [
           "View Employees",
           "View Employees by Department",
-          // "View Employees by Manager",
+          
           "Add Employee",
           "Remove Employees",
           "Update Employee Role",
           "Add Role",
-          // "Remove Role",
-          // "Update Employee Manager",
+          
           "End"]
       })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+                    case "Add Employee":
+            addEmployee();
+            break;
+          case "Remove Employees":
+            removeEmployees();
+            break;
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+          case "Add Role":
+            addRole();
+            break;
+           
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
+
+  function viewEmployee() {
+    console.log("Viewing employees\n");
+  
+    var query =
+      `SELECT e.id, e.first_name, e.last_name, r.title, d.name AS department, r.salary, CONCAT(m.first_name, ' ', m.last_name) AS manager
+    FROM employee e
+    LEFT JOIN role r
+      ON e.role_id = r.id
+    LEFT JOIN department d
+    ON d.id = r.department_id
+    LEFT JOIN employee m
+      ON m.id = e.manager_id`
+  
+    connection.query(query, function (err, res) {
+      if (err) throw err;
+  
+      console.table(res);
+      console.log("Employees viewed!\n");
+  
+      firstPrompt();
+    });
+} 
