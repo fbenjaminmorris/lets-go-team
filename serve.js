eb33bd710713880e40e33abbf50eba0925ba45ab
fbenjaminmorris <83195283+fbenjaminmorris@users.noreply.github.com>
2021-10-12 21:38:39 -0400

    update

--- serve.js
+++ serve.js
@@ -31,6 +31,55 @@ connection.connect(function (err) {
     ╚═══╩╩╩╣╔═╩═╩══╩═╗╔╩══╩══╝╚╝╚╝╚╩╝╚╩╝╚╩╝╚╩═╗╠══╩╝
     ───────║║──────╔═╝║─────────────────────╔═╝║
     ───────╚╝──────╚══╝─────────────────────╚══╝`)
-    // runs the app
+    
     firstPrompt();
 });
+
+function firstPrompt() {
+
+    inquirer
+      .prompt({
+        type: "list",
+        name: "task",
+        message: "Would you like to do?",
+        choices: [
+          "View Employees",
+          "View Employees by Department",
+          "Add Employee",
+          "Remove Employees",
+          "Update Employee Role",
+          "Add Role",
+          "End"]
+      })
+      .then(function ({ task }) {
+        switch (task) {
+          case "View Employees":
+            viewEmployee();
+            break;
+  
+          case "View Employees by Department":
+            viewEmployeeByDepartment();
+            break;
+        
+          case "Add Employee":
+            addEmployee();
+            break;
+  
+          case "Remove Employees":
+            removeEmployees();
+            break;
+  
+          case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+  
+          case "Add Role":
+            addRole();
+            break;
+  
+          case "End":
+            connection.end();
+            break;
+        }
+      });
+  }
